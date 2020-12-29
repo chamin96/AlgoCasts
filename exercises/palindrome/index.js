9500296aa90139191a63609a446d672f8e3070b7
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-12-29 13:03:22 +0530

    Update index.js

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,19 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+/* Solution 1 */
+// function palindrome(str) {
+//     return str.split('').reverse().join('') === str;
+// }
+
+/* Solution 2 */
+function palindrome(str) {
+  return str.split("").every((char, i) => {
+    debugger;
+    return char === str[str.length - i - 1];
+  });
+}
+
+palindrome('tenet');
 
 module.exports = palindrome;
