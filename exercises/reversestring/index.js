a572e297743cd4186c5475c15ec64186b3c6f125
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-12-29 12:24:55 +0530

    string reversal

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,43 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/* Solution 1 */
+// function reverse(str) {
+
+//     let reverseStr = '';
+
+//     for (let i = str.length-1; i >= 0; i--) {
+//         reverseStr += str[i];
+//     }
+
+//     return reverseStr;
+// }
+
+/* Solution 2 */
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+/* Solution 3 */
+// function reverse(str) {
+
+//     let reverseStr = '';
+
+//     for (const char of str) {
+//         reverseStr = char + reverseStr;
+//     }
+
+//     return reverseStr;
+// }
+
+/* Solution 4 */
+function reverse(str) {
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed;
+    }, '');
+}
+
+
+reverse('hello');
 
 module.exports = reverse;
